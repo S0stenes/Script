7774468e43a1eeb5bb98be7350892d4a75b8c625
S0stenes <sostenes.dev@outlook.com.br>
2022-10-23 18:00:31 -0300

    edit

--- Script/Atividade Parametro/Renomear.sh
+++ Script/Atividade Parametro/Renomear.sh
@@ -7,24 +7,25 @@ then
     echo "Esse comando faz com que renomeie todos os arquivos de uma pasta. [1] "
 elif [ $1 -eq 1 ]
 then
-    cd ~/Documentos/'Arquivos para teste'
-    ls -1 > ~/Documentos/lista-de-arquivos.txt
+    cd ~/Documentos/'Arquivos para teste' 2> /dev/null
+    ls -1 > ~/Documentos/lista-de-arquivos.txt 
+    if [ $? -ne 0 ]
+        then
+            echo "Ocorreu um problema o programa irá se encerrar..."
+            sleep 1
+            echo 'Encerrando em 3...'
+            sleep 1
+            echo 'Encerrando em 2..'
+            sleep
+            echo 'Encerrando em 1.'
+            exit
+    fi
     n=0
     while read line; do
         echo -e "$line\n";
         n=$(($n+1))
         mv "$line" "Arquivo$n"      
     done < ~/Documentos/lista-de-arquivos.txt
-elif [ $? -eq 0 ]
-then
-    echo ""
 else
-    echo "Ocorreu um problema o programa irá se encerrar..."
-    sleep 1
-    echo 'Encerrando em 3...'
-    sleep 1
-    echo 'Encerrando em 2..'
-    sleep
-    echo 'Encerrando em 1.'
-    exit
+    echo 'Digite o comando corretamente! '
 fi
